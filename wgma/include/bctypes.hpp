14671d7fe91fd0e1043844206d18faa6c8038236
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-07-20 18:35:15 -0300

    minor refactor to pmltypes and bctypes

--- wgma/include/bctypes.hpp
+++ wgma/include/bctypes.hpp
@@ -1,6 +1,6 @@
 #ifndef _BCTYPES_HPP_
 #define _BCTYPES_HPP_
-
+#include <ostream>
 
 namespace wgma{
   namespace bc{
@@ -17,10 +17,23 @@ namespace wgma{
       }
     }
 
+    std::string to_string(type t){
+      switch(t){
+      case type::PEC: return "PEC";
+      case type::PMC: return "PMC";
+      }
+    }
+
+    std::ostream& operator<<( std::ostream& out, const type& t ){
+      return out << to_string(t);
+    }
+
     //! Data structure for easier creation of PML regions
     struct data{
       type t;
       int id;
+      explicit data(int i, type tp) : id(i), t(tp) {}
+      data() : t(type::PEC), id(-100) {}
     };
   };
 };
--- wgma/include/pmltypes.hpp
+++ wgma/include/pmltypes.hpp
@@ -17,6 +17,23 @@ namespace wgma{
       xpym//!< attenuates with increasing x and decreasing y
     };
 
+    std::string to_string(type t){
+      switch(t){
+      case type::xp: return "xp";
+      case type::yp: return "yp";
+      case type::xm: return "xm";
+      case type::ym: return "ym";
+      case type::xpyp: return "xpyp";
+      case type::xmyp: return "xmyp";
+      case type::xmym: return "xmym";
+      case type::xpym: return "xpym";
+      }
+    }
+
+    std::ostream& operator<<( std::ostream& out, const type& t ){
+      return out << to_string(t);
+    }
+
     //!Queries for pml attenuation in the x-direction.
     bool attx(type t){
       if( t == type::yp || t == type::ym) return false;
@@ -51,6 +68,8 @@ namespace wgma{
       type t;
       STATE alpha;
       int id;
+      explicit data(int i, type tp, STATE a) : id(i), t(tp), alpha(a) {}
+      data() : id(-100), alpha(0), t(type::xm) {}
     };
   };
   
