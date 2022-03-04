f432553f328be5a8b44c9ebcd276b34f01462db5
Francisco Orlandini <francisco.orlandini@gmail.com>
2022-03-04 15:15:02 -0300

    feat(pml)!: AddPMLRegion takes materials as parameters
    Now one could add PML regions of arbitrary materials by means of
      #include <cmeshtools_impl.hpp>
    followed by
      AddRectangularPMLRegion<MATPML,MATVOL>(...);

--- wgma/include/cmeshtools.hpp
+++ wgma/include/cmeshtools.hpp
@@ -82,6 +82,8 @@ void SetupGmshMaterialData(const TPZVec<std::map<std::string,int>> &gmshmats,
      @brief Adds a rectangular PML region to a computational mesh.
      All the other mesh regions (excluding BCs) should have been previously inserted,
      so the function can identify to which region the PML is associated.
+     @tparam MATPML material to create in the PML
+     @tparam MATVOL material from which the PML inherits
      @param[in] matId pml material identifier.
      @param[in] alphax attenuation constant in the x-direction.
      @param[in] alphay attenuation constant in the y-direction.
@@ -91,6 +93,7 @@ void SetupGmshMaterialData(const TPZVec<std::map<std::string,int>> &gmshmats,
      @param[in] cmesh the computational mesh.
      @return This method calls FindPMLNeighbourMaterial internally.
   */
+  template<class MATPML, class MATVOL>
   void
   AddRectangularPMLRegion(const int matId,
                           const int alphax,const int alphay,
