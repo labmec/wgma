5e2444c8eb253abbcfb7461ff0f6ccb606bdf9a7
Francisco Orlandini <francisco.orlandini@gmail.com>
2022-08-11 16:58:23 -0300

    feat(examples): slab_disc mesh update
    now we add another 1D subdomain to compare the projected solution

--- examples/meshes/slab_disc.py
+++ examples/meshes/slab_disc.py
@@ -26,8 +26,9 @@ h2 = 1.5
 h_domain = 10
 w_domain = 10
 d_src = 1
+d_far = 6*d_src
 el_core = 0.15
-thickness = 2*max(h1,h2)
+thickness = 2*max(h1, h2)
 el_clad = 0.75
 d_pmlx = 3
 d_pmly = 1
@@ -93,14 +94,15 @@ modal_map_left = apply_boolean_operation(objs, tools, "fragment", True, el_clad)
 remap_tags([rec_left, rec_lcore, source_left], modal_map_left)
 
 
-#let us split the source domains
+# let us split the source domains
 src_left_clad_tags = []
 src_left_core_tags = []
 
 for tag in source_left.tag:
     up, _ = gmsh.model.get_adjacencies(1, tag)
     up = up[0]
-    src_left_clad_tags.append(tag) if up in rec_left.tag else src_left_core_tags.append(tag)
+    src_left_clad_tags.append(
+        tag) if up in rec_left.tag else src_left_core_tags.append(tag)
 
 # right domain (cladding+core)
 rec_right = RectData()
@@ -137,6 +139,29 @@ source_right_pts = gmsh.model.get_boundary(
     [(1, tag) for tag in source_right.tag],
     combined=True, oriented=False, recursive=False)
 source_right_pts = [reg[1] for reg in source_right_pts]
+# create rightmost line for comparing scattered field
+
+far_right = LineData()
+far_right.xb = d_src+d_far
+far_right.yb = -rec_right.h/2
+far_right.xe = d_src+d_far
+far_right.ye = rec_right.h/2
+create_line(far_right, el_clad)
+gmsh.model.occ.synchronize()  # for get_boundary
+far_right_pts = gmsh.model.get_boundary(
+    [(1, tag) for tag in far_right.tag],
+    combined=True, oriented=False, recursive=False)
+far_right_pts = [reg[1] for reg in far_right_pts]
+# let us ensure the same meshing will happen
+dim = 1
+affine = [1.0 if i == j else 0 for i in range(4) for j in range(4)]
+pos = {"dx": 3, "dy": 7, "dz": 11}
+val = {"dx": d_far, "dy": 0, "dz": 0}
+affine[pos["dx"]] = val["dx"]
+affine[pos["dy"]] = val["dy"]
+affine[pos["dz"]] = val["dz"]
+gmsh.model.mesh.set_periodic(dim, far_right.tag, source_right.tag, affine)
+
 
 objs = []
 [objs.append((2, s)) for s in rec_right.tag]
@@ -144,10 +169,12 @@ objs = []
 tools = []
 [tools.append((1, l)) for l in source_right.tag]
 [tools.append((0, p)) for p in source_right_pts]
+[tools.append((1, l)) for l in far_right.tag]
+[tools.append((0, p)) for p in far_right_pts]
 
 modal_map_right = apply_boolean_operation(
     objs, tools, "fragment", True, el_clad)
-remap_tags([rec_right, rec_rcore, source_right], modal_map_right)
+remap_tags([rec_right, rec_rcore, source_right, far_right], modal_map_right)
 
 
 # update new numbering after deleting duplicates
@@ -155,14 +182,25 @@ gmsh.model.occ.remove_all_duplicates()
 gmsh.model.occ.synchronize()
 
 
-#let us split the source domains
+# let us split the source domains
 src_right_clad_tags = []
 src_right_core_tags = []
 
 for tag in source_right.tag:
     up, _ = gmsh.model.get_adjacencies(1, tag)
     up = up[0]
-    src_right_clad_tags.append(tag) if up in rec_right.tag else src_right_core_tags.append(tag)
+    src_right_clad_tags.append(
+        tag) if up in rec_right.tag else src_right_core_tags.append(tag)
+
+
+far_right_clad_tags = []
+far_right_core_tags = []
+
+for tag in far_right.tag:
+    up, _ = gmsh.model.get_adjacencies(1, tag)
+    up = up[0]
+    far_right_clad_tags.append(
+        tag) if up in rec_right.tag else far_right_core_tags.append(tag)
 
 
 # split the domains for setting up the PMLs
@@ -202,6 +240,11 @@ source_right_pts = gmsh.model.get_boundary(
 source_right_pts = [reg[1] for reg in source_right_pts]
 
 
+far_right_pts = gmsh.model.get_boundary(
+    [(1, tag) for tag in far_right.tag],
+    combined=True, oriented=False, recursive=False)
+far_right_pts = [reg[1] for reg in far_right_pts]
+
 # get boundary
 dim = 2
 all_domains = gmsh.model.get_entities(dim)
@@ -211,7 +254,8 @@ scatt_bound = [bnd[1] for bnd in scatt_bound]
 # set element size per region
 
 gmsh.model.mesh.field.add("Distance", 1)
-gmsh.model.mesh.field.setNumbers(1, "SurfacesList", rec_lcore.tag + rec_rcore.tag)
+gmsh.model.mesh.field.setNumbers(
+    1, "SurfacesList", rec_lcore.tag + rec_rcore.tag)
 
 gmsh.model.mesh.field.add("Threshold", 2)
 gmsh.model.mesh.field.setNumber(2, "InField", 1)
@@ -238,15 +282,19 @@ domain_physical_ids_1d = {
     "source_core_left": 6,
     "source_clad_right": 7,
     "source_core_right": 8,
-    "scatt_bound": 9
+    "far_clad_right": 9,
+    "far_core_right": 10,
+    "scatt_bnd": 11
 }
 
 domain_physical_ids_0d = {
-    "source_left_bnd": 11,
-    "source_right_bnd": 12
+    "source_left_bnd": 12,
+    "source_right_bnd": 13,
+    "far_right_bnd":14
 }
 
-domain_physical_ids = [domain_physical_ids_0d, domain_physical_ids_1d, domain_physical_ids_2d]
+domain_physical_ids = [domain_physical_ids_0d,
+                       domain_physical_ids_1d, domain_physical_ids_2d]
 
 domain_regions = {"core_left": rec_lcore.tag,
                   "cladding_left": rec_left.tag,
@@ -258,17 +306,25 @@ domain_regions = {"core_left": rec_lcore.tag,
                   "source_core_right": src_right_core_tags,
                   "source_left_bnd": source_left_pts,
                   "source_right_bnd": source_right_pts,
-                  "scatt_bound": scatt_bound
+                  "far_clad_right": far_right_clad_tags,
+                  "far_core_right": far_right_core_tags,
+                  "far_right_bnd": far_right_pts,
+                  "scatt_bnd": scatt_bound
                   }
 
 
-insert_pml_ids(pmlmap, domain_physical_ids,domain_regions)
+insert_pml_ids(pmlmap, domain_physical_ids, domain_regions)
 
 
 generate_physical_ids(domain_physical_ids, domain_regions)
 
-
 gmsh.model.mesh.generate(2)
+# we know for sure that the elements on the minion edge are with reversed orientation
+dim = 1
+
+reverse_edges = [(dim, x) for x in far_right_core_tags + far_right_clad_tags]
+gmsh.model.mesh.reverse(reverse_edges)
+
 if __name__ == "__main__":
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
