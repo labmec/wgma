784a543e230c55297730055b4704155fcd6d0a0f
Francisco Orlandini <francisco.orlandini@gmail.com>
2022-09-08 16:59:06 -0300

    feat(wgma): adds planarwg::mode_to_string function

--- wgma/include/modetypes.hpp
+++ wgma/include/modetypes.hpp
@@ -1,10 +1,20 @@
 #ifndef _MODE_TYPES_HPP_
 #define _MODE_TYPES_HPP_
 
-namespace wgma{
-  namespace planarwg{
-    enum class mode{TE, TM};//< modes approximated in planar waveguides
+#include <string>
+
+namespace wgma::planarwg{
+  enum class mode{TE, TM};//< modes approximated in planar waveguides
+
+  //! for easier output
+  inline std::string mode_to_string(mode m){
+    switch(m){
+    case mode::TE:
+      return "TE";
+    case mode::TM:
+      return "TM";
+    }
   }
-}
+};
 
 #endif
