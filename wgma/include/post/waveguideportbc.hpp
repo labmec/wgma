1292b98e04ec1a682c20704bf9b9597e90654f61
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-08-04 17:54:30 -0300

    feat(post): introduces waveguideport bc class
    together with an approximation space restriction
    on the boundary, this class implements the
    waveguideport bc (currently only for outgoing waves)

---
+++ wgma/include/post/waveguideportbc.hpp
@@ -0,0 +1,53 @@
+#ifndef _WAVEGUIDEPORTBC_HPP_
+#define _WAVEGUIDEPORTBC_HPP_
+
+#include <post/integrator.hpp>
+
+
+namespace wgma::post{
+  /** @brief Computes the boundary terms for outgoing waveguide port boundary condition
+
+      Combined with the restriction of the approximation space, this class provides
+      the contribution for all modes to be added to the FEM matrix
+
+      Using the notation of Jian-ming Jin's Finite Element Method for Electromagnetics,
+      chapter Finite Element-Eigenfunction Expansion Methods,
+      section on Waveguide Port Boundary Condition,
+      this class will compute
+      \int_\Gamma P(E)\cdot F d\Gamma
+
+      provided that E,F are the basis functions restricted on the computed modes
+      of the waveguide.
+   */
+  template<class TSPACE>
+  class WaveguidePortBC: public TSPACE{
+  public:
+    WaveguidePortBC(TPZAutoPointer<TPZCompMesh> mesh,
+                 std::set<int> matids = {},
+                 int nThreads = 4) : TSPACE(mesh,matids,nThreads) {}
+    WaveguidePortBC(TPZAutoPointer<TPZCompMesh> mesh,
+                 TPZVec<TPZCompEl*> elvec,
+                 int nThreads = 4) : TSPACE(mesh,elvec,nThreads) {}
+    //! Computes contribution of all modes in mesh
+    void ComputeContribution();
+    //! Gets contributions associated with each mode of the waveguide
+    void GetContribution(TPZVec<CSTATE> &km_vec) {km_vec = m_res;}
+    //! Initialises element data and sets material
+    void InitData(TPZCompEl *el, ElData &data) override;
+    //! Sets beta values
+    void SetBeta(const TPZVec<CSTATE> &beta){m_beta = beta;}
+    //! Gets beta values
+    void GetBeta(TPZVec<CSTATE> &beta) const {beta = m_beta;}
+  protected:
+    //! Computes contribution at an integration point
+    void Compute(const ElData &data, REAL weight, int thread) override;
+    //! Temp results (one position for thread)
+    TPZVec<TPZVec<CSTATE>> m_scratch;
+    //! Results (one position for solution)
+    TPZVec<CSTATE> m_res;
+    //! Propagation constant beta (needed for 2D modal analysis)
+    TPZVec<CSTATE> m_beta;
+  };
+};
+
+#endif /* _WAVEGUIDEPORTBC_HPP_ */
---
+++ wgma/src/post/waveguideportbc.cpp
@@ -0,0 +1,96 @@
+#include "post/waveguideportbc.hpp"
+#include <TPZBndCond.h>
+#include <TPZMaterialDataT.h>
+#include <Electromagnetics/TPZScalarField.h>
+#include <Electromagnetics/TPZWgma.h>
+#include <TPZElectromagneticConstants.h>
+#include <pzvec_extras.h>
+#include <pzaxestools.h>
+using namespace std::complex_literals;
+
+namespace wgma::post{
+
+  template<class TSPACE>
+  void WaveguidePortBC<TSPACE>::ComputeContribution(){
+    const int size_res = std::max(this->NThreads(),1);
+    m_scratch.Resize(size_res);
+    
+    const int nsol = this->Mesh()->Solution().Cols();
+    m_res.Resize(nsol);
+    m_res.Fill(0);
+    for(auto &res : m_scratch) {res.Resize(nsol);}
+    this->Integrate(this->m_elvec);
+    
+    for (const auto &res : m_scratch){
+      for(auto isol = 0; isol < nsol; isol++){
+        m_res[isol] += res[isol];
+      }
+    }
+    m_scratch.Resize(0);
+  }
+
+  template<class TSPACE>
+  void WaveguidePortBC<TSPACE>::InitData(TPZCompEl *el, ElData &data)
+  {
+    TSPACE::InitData(el,data);
+    data.SetMaterial(el->Material());
+    if constexpr(std::is_same_v<TSPACE,SingleSpaceIntegrator>){
+      TPZMaterialDataT<CSTATE> &eldata = data;
+      eldata.fNeedsNormal = true;
+    }else{
+      TPZVec<TPZMaterialDataT<CSTATE>>& datavec = data;
+      for (auto &d : datavec){
+        d.fNeedsNormal= true;
+      }
+    }
+  }
+  
+  template<class TSPACE>
+  void WaveguidePortBC<TSPACE>::Compute(const ElData &eldata, REAL weight, int index)
+  {   
+    if constexpr(std::is_same_v<TSPACE,SingleSpaceIntegrator>){
+      const TPZMaterialDataT<CSTATE> &data = eldata;
+      const auto &sol =  data.sol;
+      const auto solsize = sol.size();
+      if(solsize != m_beta.size()){
+        DebugStop();
+      }
+      STATE val = 0;
+      for(auto isol = 0; isol < solsize; isol++){
+        const auto beta = m_beta[isol];
+        const CSTATE val =  1i*beta* sol[isol][0] * sol[isol][0];
+        this->m_scratch[index][isol] += weight * fabs(data.detjac) * val;
+      }
+    }else{
+      //we expect a TPZWgma material
+      const TPZVec<TPZMaterialDataT<CSTATE>> &datavec = eldata;
+
+      TPZManVector<CSTATE,3> et(3,0.);
+      TPZFNMatrix<3,CSTATE> grad_ez(3,1,0.);
+
+      const int solsize = datavec[0].sol.size();
+      const auto &axes = datavec[0].axes;
+      const auto detjac = datavec[0].detjac;
+      STATE val = 0;
+      for(auto isol = 0; isol < solsize; isol++){
+        et = datavec[ TPZWgma::HCurlIndex() ].sol[isol];
+        const TPZFMatrix<CSTATE> &dsoldaxes = datavec[ TPZWgma::H1Index()].dsol[isol];
+        TPZAxesTools<CSTATE>::Axes2XYZ(dsoldaxes, grad_ez, axes);
+        CSTATE val = 0;
+        const auto beta = m_beta[isol];
+        for(int ix = 0; ix < 3; ix++) {
+          grad_ez(ix,0) *= 1i;
+          et[ix] /= beta;
+          val += (1i*beta*et[ix]+grad_ez.Get(ix,0))*et[ix];
+        }
+        this->m_scratch[index][isol] += weight * fabs(detjac) * val;
+      }
+    }
+  }
+
+  template
+  class WaveguidePortBC<SingleSpaceIntegrator>;
+  template
+  class WaveguidePortBC<MultiphysicsIntegrator>;
+  
+};
