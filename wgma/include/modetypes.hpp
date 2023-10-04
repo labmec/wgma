12d7b07b207ca3e01cf7370744b58f8c80d7d154
orlandini <francisco.orlandini@gmail.com>
2023-10-04 16:45:43 -0300

    feat(modetypes): adds string_to_mode method + Invalid enum val

--- wgma/include/modetypes.hpp
+++ wgma/include/modetypes.hpp
@@ -2,18 +2,34 @@
 #define _MODE_TYPES_HPP_
 
 #include <string>
+#include <map>
 
 namespace wgma::planarwg{
-  enum class mode{TE, TM};//< modes approximated in planar waveguides
+  enum class mode{Invalid,TE, TM};//< modes approximated in planar waveguides
 
   //! for easier output
   inline std::string mode_to_string(mode m){
     switch(m){
+    case mode::Invalid:
+      return "Invalid";
     case mode::TE:
       return "TE";
     case mode::TM:
       return "TM";
     }
+  }
+  //! for easier input
+  inline mode string_to_mode(const std::string name){
+    static const std::map<std::string,mode> stringvals{
+      {"TE",mode::TE},
+      {"TM",mode::TM}
+    };
+
+    auto itr = stringvals.find(name);
+    if( itr != stringvals.end() ) {
+      return itr->second;
+    }
+    return mode::Invalid; 
   }
 };
 
