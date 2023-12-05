b2d042be26b06dc49ceaa0eb2cd08f83e6196534
orlandini <francisco.orlandini@gmail.com>
2023-12-05 11:41:46 -0300

    cylindrical sf3d mesh (only one radius)

--- examples/meshes/sf3d_new.py
+++ examples/meshes/sf3d_new.py
@@ -7,19 +7,17 @@ from math import ceil
 from utils.gmsh import (
     add_cylindrical_regions,
     apply_boolean_operation,
-    BoxData,
+    RectData,
+    CircleData,
     CylinderData,
-    create_box,
+    create_circle,
     create_rect,
-    find_pml_region,
     generate_physical_ids,
+    get_neighbours,
     insert_pml_ids,
     remap_tags,
-    RectData,
     split_region_dir,
-    VolData,
-    create_pml_region,
-    create_pml_corner
+    create_pml_region
 )
 
 
@@ -47,7 +45,7 @@ ncore = 1.4457
 
 r_core = 8  # core radius
 # distance from center to end of cladding region(inner box)
-d_box = r_core + 3.5 * wl/nclad
+r_box = r_core + 3.5 * wl/nclad
 l_domain = 0.25*wl
 d_pml = 1.75*wl/nclad  # pml width
 nel_l = 5  # number of elements / wavelength
@@ -55,10 +53,6 @@ nel_l = 5  # number of elements / wavelength
 el_clad = (wl/nclad)/nel_l  # el size in cladding
 el_core = (wl/ncore)/nel_l  # el size in core
 
-# # element size in the core
-# el_core = 0.75 * lambda_core
-# # element size in the cladding
-# el_clad = 0.75 * lambda_clad
 
 # pml width
 d_pmlx = d_pml
@@ -81,22 +75,22 @@ gmsh.logger.start()
 
 # outer cylinder (PML)
 pml = CylinderData()
-pml.xc = [0, 0, -l_domain]
+pml.xc = [0, 0, -l_domain/2]
 pml.axis = [0, 0, l_domain]
-pml.radius = d_box+d_pml
+pml.radius = r_box+d_pml
 pml.tag = [gmsh.model.occ.add_cylinder(
     *pml.xc, *pml.axis, pml.radius)]
 
 clad = CylinderData()
-clad.xc = [0, 0, -l_domain]
+clad.xc = [0, 0, -l_domain/2]
 clad.axis = [0, 0, l_domain]
-clad.radius = d_box
+clad.radius = r_box
 clad.tag = [gmsh.model.occ.add_cylinder(
     *clad.xc, *clad.axis, clad.radius)]
 
 
 core = CylinderData()
-core.xc = [0, 0, -l_domain]
+core.xc = [0, 0, -l_domain/2]
 core.axis = [0, 0, l_domain]
 core.radius = r_core
 core.tag = [gmsh.model.occ.add_cylinder(
@@ -122,16 +116,62 @@ clad_map = apply_boolean_operation(objs, tools, "cut", False, el_clad)
 remap_tags([clad], clad_map)
 gmsh.model.occ.remove_all_duplicates()
 gmsh.model.occ.synchronize()
+
+# now we create a plane for analysing the solution (probe)
+probe = CircleData()
+probe.xc = 0
+probe.yc = 0
+probe.zc = 0
+probe.radius = r_box+d_pml
+
+create_circle(probe, el_clad)
+
+gmsh.model.occ.remove_all_duplicates()
+gmsh.model.occ.synchronize()
+
+#we divide it at the y=0 plane to avoid issues when finding the PML
+horiz_plane = RectData()
+horiz_plane.xc = -(r_box+d_pml)
+horiz_plane.yc = 0
+horiz_plane.zc = -l_domain/2
+horiz_plane.h = 2*(r_box+d_pml)
+horiz_plane.w = l_domain
+
+create_rect(horiz_plane, el_clad,'y')
+
+gmsh.model.occ.remove_all_duplicates()
+gmsh.model.occ.synchronize()
+
+cut_vol_with_plane([pml, clad, core], [probe,horiz_plane], el_clad)
+
+
 # now we create the PMLs in the z-direction
 # first split the domains for setting up the PMLs
 dim = 3
 vol_domains = gmsh.model.get_entities(dim)
 zm, zp = split_region_dir(vol_domains, 'z')
 
-if len(pml.tag) != 1 or len(clad.tag) != 1:
-    raise Exception("we expect only one tag for clad and radial pml domains")
+# now let us see which PML is adjacent to each cladding section
+pmlmap = {}
+for tag in pml.tag:
+    found = False
+    # we iterate over its boundaries
+    for bnd in gmsh.model.get_boundary([(dim, tag)], oriented=False):
+        if found:
+            break
+        # we iterate over its neighbours
+        neighs, _ = gmsh.model.get_adjacencies(bnd[0], bnd[1])
+        i = set.intersection(set(neighs), set(clad.tag))
+        if len(i) > 0:
+            if len(i) > 1:
+                # how can we have two cladding domains as neighbours?
+                raise Exception("we expect only one result here")
+            found = True
+            pmlmap[('rp', tag)] = i.pop()
 
-pmlmap = {('rp', pml.tag[0]): clad.tag[0]}
+if len(pmlmap) != len(pml.tag):
+    raise Exception("could not find all PML neighbours")
+
 pmlmap.update(create_pml_region(zp, "zp", d_pmlz, nlayerspml))
 pmlmap.update(create_pml_region(zm, "zm", d_pmlz, nlayerspml))
 # now we filter the cylindrical PML region
@@ -149,10 +189,103 @@ for (pml_type, pml_id), pml_neigh in pmlmap.items():
         new_pml_map.update({(pml_type, pml_id): pml_neigh})
 pmlmap = new_pml_map
 
-print(pmlmap)
 gmsh.model.occ.remove_all_duplicates()
 gmsh.model.occ.synchronize()
+# now we need to find the 2D domains
+dim = 3
+
+
+def get_boundary_in_z_dir(dt, direction):
+    zm, zp = split_region_dir(dt, 'z')
+    vol = zp if direction == '+' else zm
+    bnd = gmsh.model.get_boundary(vol, combined=True, oriented=False)
+    zm, zp = split_region_dir(bnd, 'z', True)
+    res = {}
+    res['left_entities'] = [t for _,t in zm]
+    res['right_entities'] = [t for _,t in zp]
+    return res
+
+
+clad_res = get_boundary_in_z_dir([(dim, t) for t in clad.tag], '-')
+src_left_clad = clad_res['left_entities']
+probe_clad = clad_res['right_entities']
+
+core_res = get_boundary_in_z_dir([(dim, t) for t in core.tag], '-')
+src_left_core = core_res['left_entities']
+probe_core = core_res['right_entities']
+
+pml_res = get_boundary_in_z_dir([(dim, t) for t in pml.tag], '-')
+src_left_pml = pml_res['left_entities']
+probe_pml = pml_res['right_entities']
+
+clad_res = get_boundary_in_z_dir([(dim, t) for t in clad.tag], '+')
+src_right_clad = clad_res['right_entities']
+
+core_res = get_boundary_in_z_dir([(dim, t) for t in core.tag], '+')
+src_right_core = core_res['right_entities']
+
+pml_res = get_boundary_in_z_dir([(dim, t) for t in pml.tag], '+')
+src_right_pml = pml_res['right_entities']
+
+# let us config the boundary conditions
+dim = 3
+all_domains = gmsh.model.get_entities(dim)
+all_bounds = [t for _, t in gmsh.model.get_boundary(
+    all_domains, combined=True, oriented=False, recursive=False)]
+
+
+src_left_domains = src_left_clad+src_left_core+src_left_pml
+src_left_bnd = [t for _, t in gmsh.model.get_boundary(
+    [(2, tag) for tag in src_left_domains])]
+
+src_right_domains = src_right_clad+src_right_core+src_right_pml
+src_right_bnd = [t for _, t in gmsh.model.get_boundary(
+    [(2, tag) for tag in src_right_domains])]
+
+probe_domains = probe_clad+probe_core+probe_pml
+probe_bnd = [t for _, t in gmsh.model.get_boundary(
+    [(2, tag) for tag in probe_domains])]
+
+# let us find the tag of all cylindrical surfaces
+
+
+def find_common_bounds(domain_1_tags, domain_2_tags, dim):
+    domain_1_tags_bnd = [t for _, t in gmsh.model.get_boundary(
+        [(dim, tag) for tag in domain_1_tags],
+        combined=False, oriented=False)]
+    domain_2_tags_bnd = [t for _, t in gmsh.model.get_boundary(
+        [(dim, tag) for tag in domain_2_tags],
+        combined=False, oriented=False)]
+    common_bnd = [t for t in domain_1_tags_bnd if t in domain_2_tags_bnd]
+    return common_bnd
+
+
+#given a list of surface tags, it filters only entities of type cylinder
+def filter_cyl_tags(taglist):
+    return [t for t in taglist if gmsh.model.get_type(2,t) == "Cylinder"]
+#given a list of cylinder tags, it keeps searching for neighbours
+#that are also cylinders.
+def find_all_cyl_tags(cyltags):
+    while True:
+        cylset = set(cyltags)
+        new_cyls = set(filter_cyl_tags(get_neighbours(cyltags,2)))
+        if len(cylset) == len(cylset.union(new_cyls)):
+            break
+        cyltags = list(cylset.union(new_cyls))
+    return cyltags
+
+cyl1 = find_all_cyl_tags(find_common_bounds(core.tag, clad.tag, 3))
+cyl2 = find_all_cyl_tags(find_common_bounds(clad.tag, pml.tag, 3))
+cyl3 = filter_cyl_tags(all_bounds)
+
+core.surftag = cyl1
+clad.surftag = cyl2
+pml.surftag = cyl3
+
+all_cyl_data = [core, clad,pml]
+
 # element sizes
+
 
 small_domains = core.tag
 dim = 3
@@ -163,9 +296,7 @@ gmsh.model.mesh.field.set_numbers(field_ct, "VolumesList", small_domains)
 gmsh.model.mesh.field.set_number(field_ct, "VIn", el_core)
 gmsh.model.mesh.field.set_number(field_ct, "VOut", el_clad)
 
-# gmsh.model.mesh.field.add("Min", field_ct)
-# gmsh.model.mesh.field.set_numbers(
-#     field_ct, "FieldsList", [i for i in range(1, field_ct)])
+
 gmsh.model.mesh.field.setAsBackgroundMesh(field_ct)
 gmsh.option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
 gmsh.option.setNumber("Mesh.MeshSizeFromPoints", 0)
@@ -176,13 +307,21 @@ domain_physical_ids_3d = {
     "cladding": 2
 }
 domain_physical_ids_2d = {
-    # "src_core": 3,
-    # "src_clad": 4,
-    # "scatt_bnd": 10
+    "src_left_core": 3,
+    "src_left_clad": 4,
+    "pml_src_left_clad_rp": 5,
+    "src_right_core": 6,
+    "src_right_clad": 7,
+    "pml_src_right_clad_rp": 8,
+    "probe_core": 9,
+    "probe_clad": 10,
+    "pml_probe_clad_rp": 11,
+    "scatt_bnd": 12
 }
-
 domain_physical_ids_1d = {
-    # "modal_bnd": 11
+    "src_left_bnd": 13,
+    "src_right_bnd": 14,
+    "probe_bnd": 15
 }
 
 domain_physical_ids_0d = {}
@@ -191,18 +330,32 @@ domain_physical_ids = [domain_physical_ids_0d, domain_physical_ids_1d,
                        domain_physical_ids_2d, domain_physical_ids_3d]
 
 domain_regions = {"core": core.tag,
-                  "cladding": clad.tag
+                  "cladding": clad.tag,
+                  "src_left_core": src_left_core,
+                  "src_left_clad": src_left_clad,
+                  "pml_src_left_clad_rp": src_left_pml,
+                  "src_right_core": src_right_core,
+                  "src_right_clad": src_right_clad,
+                  "pml_src_right_clad_rp": src_right_pml,
+                  "probe_core": probe_core,
+                  "probe_clad": probe_clad,
+                  "pml_probe_clad_rp": probe_pml,
+                  "src_left_bnd": src_left_bnd,
+                  "src_right_bnd": src_right_bnd,
+                  "probe_bnd": probe_bnd,
+                  "scatt_bnd": all_bounds
                   }
+
+add_cylindrical_regions(all_cyl_data, domain_physical_ids, domain_regions)
 
 insert_pml_ids(pmlmap, domain_physical_ids, domain_regions)
 generate_physical_ids(domain_physical_ids, domain_regions)
-
 
 gmsh.fltk.run()
 
 gmsh.model.mesh.generate(3)
 
-# gmsh.model.mesh.optimize("Netgen")
+gmsh.model.mesh.optimize("Netgen")
 
 if __name__ == "__main__":
     abspath = os.path.abspath(__file__)
