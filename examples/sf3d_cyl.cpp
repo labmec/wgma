b36c6a3b127e87c4395b736c1569b5e50f9e0f7a
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-12-07 16:01:19 -0300

    sf3d cyl target (modal analysis)

---
+++ examples/sf3d_cyl.cpp
@@ -0,0 +1,470 @@
+/**
+slab_disc.cpp
+
+This target performs the modal analysis of a dielectric slab
+and then the subsequent scattering analysis at a waveguide discontinuity.
+***/
+
+// wgma includes
+#include "cmeshtools.hpp"
+#include "gmeshtools.hpp"
+#include "pmltypes.hpp"
+#include <TPZEigenAnalysis.h>
+#include <wganalysis.hpp>
+#include <scattering.hpp>
+#include <util.hpp>
+#include <slepcepshandler.hpp>
+#include <post/orthosol.hpp>
+#include <post/solutionnorm.hpp>
+#include <post/waveguideportbc.hpp>
+#include <post/waveguidecoupling.hpp>
+// pz includes
+#include <MMeshType.h>      //for MMeshType
+#include <TPZSimpleTimer.h> //for TPZSimpleTimer
+#include <pzlog.h>          //for TPZLogger
+#include <TPZMaterial.h>
+#include <TPZVTKGenerator.h>
+#include <pzinterpolationspace.h>
+
+#include <regex>//for string search
+#include <thread>
+
+
+//!minimum shared sim data
+struct SimData{
+  //!.msh mesh
+  std::string meshfile;
+  //!.csv file containing cylinder info
+  std::string cylfile;
+  //!wavelength
+  STATE lambda{1.5};
+  //!geometric scaling (floating point precision)
+  REAL scale{1};
+  //!reffractive index of core
+  STATE ncore{1};
+  //!reffractive index of clad
+  STATE nclad{1};
+  //!polynomial order
+  int porder{-1};
+  //!pml attenuation constant in radial direction
+  CSTATE alphaPMLr{0};
+  //!pml attenuation constant in z-direction
+  CSTATE alphaPMLz{0};
+  //!whether to filter dirichlet eqs
+  bool filterBoundEqs{true};
+  //!renumber equations
+  bool optimizeBandwidth{true};
+  //!output geometric mesh in .txt and .vtk files
+  bool printGmesh{false};
+  //!post process fields
+  bool exportVtk{false};
+  //!whether to compute coupling mat
+  bool couplingmat{false};
+  //!vtk resolution
+  int vtkRes{0};
+  //!number of threads
+  int nThreads{(int)std::thread::hardware_concurrency()};
+  //!prefix for both meshes and output files
+  std::string prefix{""};
+};
+
+//!needed data from modal analysis to create waveguide port bc
+struct WgbcData{
+  TPZAutoPointer<TPZCompMesh> cmesh;
+  TPZFMatrix<CSTATE> wgbc_k;
+  TPZVec<CSTATE> wgbc_f;
+};
+
+
+// Sets geometric info regarding all the cylinders in the mesh
+TPZVec<wgma::gmeshtools::CylinderData> SetUpCylData(std::string_view filename,
+                                                    const REAL scale);
+TPZAutoPointer<wgma::wganalysis::Wgma2D>
+ComputeModalAnalysis(
+  TPZAutoPointer<TPZGeoMesh> gmesh,
+  TPZVec<std::map<std::string, int>> gmshmats,
+  const SimData& simdata,
+  const CSTATE target,
+  const int nEigenpairs,
+  const TPZEigenSort sortingRule,
+  bool usingSLEPC,
+  const std::string &name);
+
+SimData GetSimData()
+{
+
+  // path for output files
+  const std::string path {"res_sf3d_cyl/"};
+  // common prefix for both meshes and output files
+  const std::string basisName{"sf3d_cyl"};
+  // prefix for exported files
+  const std::string prefix{path+basisName};
+  
+  SimData data;
+  data.meshfile="meshes/sf3d_cyl.msh";
+  data.cylfile="meshes/sf3d_cyl_cyldata.csv";
+  data.lambda = 4.00;
+  /*
+    Given the small dimensions of the domain, scaling it can help in
+    achieving good precision. Using 1./k0 as a scale factor results in
+    the eigenvalues -(propagationConstant/k0)^2 = -effectiveIndex^2.
+    This scale factor is often referred to as characteristic length
+    of the domain.
+  */
+  data.scale = data.lambda/(2*M_PI);
+  data.ncore = 1.4457;
+  data.nclad = 1.4378;
+  data.alphaPMLr = {sqrt(0.4*0.4+0.4*0.4), 0.0};
+  data.alphaPMLz = {0.4, 0.0};
+  data.porder = 1;
+  data.filterBoundEqs = true;
+  data.printGmesh=true;
+  data.exportVtk = true;
+  data.couplingmat = true;
+  data.vtkRes=0;
+  data.prefix = prefix;
+  return std::move(data);
+}
+
+int main(int argc, char *argv[]) {
+
+#ifdef PZ_LOG
+  /**if the NeoPZ library was configured with log4cxx,
+   * the log should be initialised as:*/
+  TPZLogger::InitializePZLOG();
+#endif
+  const SimData simdata=GetSimData();
+
+  //just to make sure we will output results
+  wgma::util::CreatePath(wgma::util::ExtractPath(simdata.prefix));
+  
+  /******************
+   * eigensolver options *
+   ******************/
+
+  // how to sort eigenvalues
+  constexpr TPZEigenSort sortingRule {TPZEigenSort::TargetRealPart};
+  bool usingSLEPC {true};
+  constexpr int nEigenpairs_left{500};
+  constexpr int nEigenpairs_right{500};
+  //if we put the exact target PETSc will complain about zero pivot in LU factor
+  const CSTATE target{-1.0000001*simdata.ncore*simdata.ncore};
+
+  /*********
+   * begin *
+   *********/
+
+
+
+
+  /*************
+   * geometry  *
+   ************/
+  // scoped-timer
+  TPZSimpleTimer total("Total");
+
+  TPZVec<std::map<std::string, int>> gmshmats;
+  constexpr bool verbosity_lvl{false};
+  auto gmesh = wgma::gmeshtools::ReadGmshMesh(simdata.meshfile, simdata.scale,
+                                              gmshmats,
+                                              verbosity_lvl);
+
+  /**
+     in order to exactly represent all the circles in the mesh,
+     the python script that generated the mesh also generated this .csv file
+    **/
+    auto cyldata = SetUpCylData(simdata.cylfile, simdata.scale);
+    wgma::gmeshtools::SetExactCylinderRepresentation(gmesh, cyldata);
+    
+  // print wgma_gmesh to .txt and .vtk format
+  if (simdata.printGmesh) {
+    // prefix for the wgma_gmesh files
+    const std::string filename = simdata.prefix + "_gmesh";
+    wgma::gmeshtools::PrintGeoMesh(gmesh, filename);
+  }
+
+
+  /********************************
+   * cmesh(modal analysis):left   *
+   ********************************/
+
+  auto modal_l_an = ComputeModalAnalysis(gmesh, gmshmats, simdata,
+                                         target, nEigenpairs_left,
+                                         sortingRule, usingSLEPC, "left");
+  
+  /********************************
+   * cmesh(modal analysis):right   *
+   ********************************/
+
+  auto modal_r_an = ComputeModalAnalysis(gmesh, gmshmats, simdata,
+                                         target, nEigenpairs_left,
+                                         sortingRule, usingSLEPC, "right");
+
+  //SolveScattering(gmesh, modal_l_an,  modal_r_an, gmshmats, simdata);
+  return 0;
+}
+
+// Sets geometric info regarding all the cylindes in the mesh
+TPZVec<wgma::gmeshtools::CylinderData> SetUpCylData(std::string_view filename,
+                                               const REAL scale) {
+  std::ifstream read(filename.data());
+  if (!read) {
+    std::cout << "Couldn't open the file " << filename << std::endl;
+    DebugStop();
+  }
+
+  auto getNextLineAndSplitIntoTokens =
+      [](std::istream &str) -> std::vector<std::string> {
+    std::vector<std::string> result;
+    std::string line;
+    std::getline(str, line);
+
+    std::stringstream lineStream(line);
+    std::string cell;
+
+    while (std::getline(lineStream, cell, ',')) {
+      result.push_back(cell);
+    }
+    // This checks for a trailing comma with no data after it.
+    if (!lineStream && cell.empty()) {
+      // If there was a trailing comma then add an empty element.
+      result.push_back("");
+    }
+    return result;
+  };
+
+  auto line = getNextLineAndSplitIntoTokens(read); // header
+  line = getNextLineAndSplitIntoTokens(read);
+  // we expect xc, yc, zc, xaxis, yaxis, zaxis, r, and matid
+  TPZVec<wgma::gmeshtools::CylinderData> cyls;
+  const auto factor = 1./scale;
+  while (line.size() == 8) {
+    wgma::gmeshtools::CylinderData cyl;
+    cyl.m_xc = std::stod(line[0]) * factor;
+    cyl.m_yc = std::stod(line[1]) * factor;
+    cyl.m_zc = std::stod(line[2]) * factor;
+    cyl.m_xaxis = std::stod(line[3]) * factor;
+    cyl.m_yaxis = std::stod(line[4]) * factor;
+    cyl.m_zaxis = std::stod(line[5]) * factor;
+    cyl.m_radius = std::stod(line[6]) * factor;
+    cyl.m_matid = std::stoi(line[7]);
+    const int ncyls = cyls.size();
+    cyls.Resize(ncyls + 1);
+    cyls[ncyls] = cyl;
+
+    line = getNextLineAndSplitIntoTokens(read);
+  }
+  return cyls;
+}
+
+
+void ComputeModes(wgma::wganalysis::Wgma2D &an,
+                  const int nThreads);
+
+void PostProcessModes(wgma::wganalysis::Wgma2D &an,
+                      std::string filename,
+                      const int vtkres,
+                      std::set<int> sols = {});
+
+
+TPZAutoPointer<TPZEigenSolver<CSTATE>>
+SetupSolver(const CSTATE target, const int nEigen,
+            TPZEigenSort sorting, bool &usingSLEPC);
+
+
+TPZAutoPointer<wgma::wganalysis::Wgma2D>
+ComputeModalAnalysis(
+  TPZAutoPointer<TPZGeoMesh> gmesh,
+  TPZVec<std::map<std::string, int>> gmshmats,
+  const SimData& simdata,
+  const CSTATE target,
+  const int nEigenpairs,
+  const TPZEigenSort sortingRule,
+  bool usingSLEPC,
+  const std::string &name)
+{
+  auto modal_cmesh = [gmesh,&gmshmats,&simdata,name](){
+    // setting up cmesh data
+    const auto &nclad = simdata.nclad;
+    const auto &ncore = simdata.ncore;
+    const auto &alphaPMLr = simdata.alphaPMLr;
+    const auto &pOrder = simdata.porder;
+    const auto &lambda = simdata.lambda;
+    const auto &scale = simdata.scale;
+    wgma::cmeshtools::PhysicalData modal_data;
+
+    std::map<std::string, std::pair<CSTATE, CSTATE>> modal_mats;
+    modal_mats["src_"+name+"_clad"] = std::make_pair<CSTATE, CSTATE>(nclad*nclad, 1.);
+    modal_mats["src_"+name+ "_core"] = std::make_pair<CSTATE, CSTATE>(ncore*ncore, 1.);
+    std::map<std::string, wgma::bc::type> modal_bcs;
+    modal_bcs["src_"+name+"_bnd"] = wgma::bc::type::PEC;
+    //dimension of the modal analysis 
+    constexpr int modal_dim{2};
+    wgma::cmeshtools::SetupGmshMaterialData(gmshmats, modal_mats, modal_bcs,
+                                            {alphaPMLr,0},
+                                            modal_data, modal_dim);
+    //we must now filter the 2D PMLs
+    std::vector<TPZAutoPointer<wgma::pml::data>>  pmlvec;
+    for(const auto &pml : modal_data.pmlvec){
+      const std::string pattern{"src_"+name+"_clad"};
+      const auto rx = std::regex{pattern, std::regex_constants::icase };
+    
+      const bool found_pattern = std::regex_search(*(pml->names.begin()), rx);
+      if(found_pattern){pmlvec.push_back(pml);}
+    }
+    modal_data.pmlvec = pmlvec;
+    return wgma::wganalysis::CMeshWgma2D(gmesh,pOrder, modal_data,lambda, scale);
+  }();
+
+  TPZAutoPointer<wgma::wganalysis::Wgma2D>
+    modal_an = new wgma::wganalysis::Wgma2D(modal_cmesh, simdata.nThreads,
+                                              simdata.optimizeBandwidth,
+                                              simdata.filterBoundEqs);
+  {
+    auto solver =
+      SetupSolver(target, nEigenpairs, sortingRule, usingSLEPC);
+    modal_an->SetSolver(*solver);
+  }
+  std::string modal_file{simdata.prefix+"_modal_"+name};
+  if(usingSLEPC){
+    modal_file += "_slepc";
+  }else{
+    modal_file += "_krylov";
+  }
+  ComputeModes(*modal_an, simdata.nThreads);
+  if(simdata.exportVtk){
+    PostProcessModes(*modal_an, modal_file, simdata.vtkRes);
+  }
+  return modal_an;
+}
+
+
+#include <slepcepshandler.hpp>
+#include <TPZKrylovEigenSolver.h>
+//utility functions
+TPZAutoPointer<TPZEigenSolver<CSTATE>>
+SetupSolver(const CSTATE target,const int neigenpairs,
+            TPZEigenSort sorting, bool &usingSLEPC)
+{
+
+#ifndef WGMA_USING_SLEPC
+  if(usingSLEPC){
+    std::cout<<"wgma was not configured with slepc. defaulting to: "
+             <<"TPZKrylovSolver"<<std::endl;
+    usingSLEPC = false;
+  }
+#endif
+
+  TPZAutoPointer<TPZEigenSolver<CSTATE>> solver{nullptr};
+  const int krylovDim{5*neigenpairs};
+  if (usingSLEPC){
+    using namespace wgma::slepc;
+    /*
+      The following are suggested SLEPc settings.
+      NOTE: -1 stands for PETSC_DECIDE
+    */
+    
+    constexpr STATE eps_tol = 1e-18;//PETSC_DECIDE
+    constexpr int eps_max_its = -1;//PETSC_DECIDE
+    constexpr EPSConv eps_conv_test = EPSConv::EPS_CONV_REL;
+    constexpr EPSWhich eps_which = EPSWhich::EPS_TARGET_REAL;
+    
+    constexpr PC pc = PC::LU;
+    constexpr KSPSolver linsolver = KSPSolver::PREONLY;
+    constexpr STATE ksp_rtol = -1;//PETSC_DECIDE
+    constexpr STATE ksp_atol = -1;//PETSC_DECIDE
+    constexpr STATE ksp_dtol = -1;//PETSC_DECIDE
+    constexpr STATE ksp_max_its = -1;//PETSC_DECIDE
+    constexpr bool eps_true_residual = false;
+    constexpr EPSProblemType eps_prob_type = EPSProblemType::EPS_GNHEP;//do NOT change
+    constexpr EPSType eps_solver_type = EPSType::KRYLOVSCHUR;
+    constexpr bool eps_krylov_locking = true;
+    constexpr STATE eps_krylov_restart = 0.7;
+    constexpr STATE eps_mpd = -1;//PETSC_DECIDE
+    constexpr bool eps_verbosity = true;
+    
+    
+    auto eps_solver = new EPSHandler<CSTATE>;
+    eps_solver->SetType(eps_solver_type);
+    eps_solver->SetProblemType(eps_prob_type);
+    eps_solver->SetEPSDimensions(neigenpairs, krylovDim, eps_mpd);
+    eps_solver->SetWhichEigenpairs(eps_which);
+    eps_solver->SetTarget(target);
+    eps_solver->SetTolerances(eps_tol,eps_max_its);
+    eps_solver->SetConvergenceTest(eps_conv_test);
+    eps_solver->SetKrylovOptions(eps_krylov_locking,eps_krylov_restart);
+    eps_solver->SetVerbose(eps_verbosity);
+    eps_solver->SetTrueResidual(eps_true_residual);
+    
+    eps_solver->SetLinearSolver(linsolver);
+    eps_solver->SetLinearSolverTol(ksp_rtol,ksp_atol,ksp_dtol,ksp_max_its);
+    eps_solver->SetPrecond(pc, 1e-16);
+
+    solver = eps_solver;
+  }else{
+    auto krylov_solver = new TPZKrylovEigenSolver<CSTATE>;
+    TPZSTShiftAndInvert<CSTATE> st;
+    krylov_solver->SetSpectralTransform(st);
+    krylov_solver->SetTarget(target);
+    krylov_solver->SetKrylovDim(krylovDim);
+    krylov_solver->SetNEigenpairs(neigenpairs);
+    krylov_solver->SetAsGeneralised(true);
+    
+    solver = krylov_solver;
+  }
+  
+  solver->SetEigenSorting(sorting);
+  return solver;
+}
+
+
+void ComputeModes(wgma::wganalysis::Wgma2D &an,
+                  const int nThreads)
+{
+  
+  TPZSimpleTimer analysis("Modal analysis");
+  static constexpr bool computeVectors{true};
+  
+  an.Run(computeVectors);
+
+  an.LoadAllSolutions();
+  auto cmesh = an.GetMesh();
+  constexpr bool conj{false};
+  //leave empty for all valid matids
+  std::set<int> matids {};
+  wgma::post::SolutionNorm<wgma::post::MultiphysicsIntegrator>(cmesh,matids,conj,nThreads).Normalise();
+
+  
+  TPZFMatrix<CSTATE> &mesh_sol=cmesh->Solution();
+  //we update analysis object
+  an.SetEigenvectors(mesh_sol);
+  an.LoadAllSolutions();
+}
+void PostProcessModes(wgma::wganalysis::Wgma2D &an,
+                      std::string filename,
+                      const int vtkres,
+                      std::set<int> sols){
+  TPZSimpleTimer postProc("Post processing");
+    
+  const std::string file = filename;
+  TPZVec<std::string> fvars = {
+      "Ez_real",
+      "Ez_abs",
+      "Et_real",
+      "Et_abs"};
+  auto cmesh = an.GetMesh();
+  auto vtk = TPZVTKGenerator(cmesh, fvars, file, vtkres);
+
+  if(sols.size() == 0){
+    const auto nsol = an.GetEigenvectors().Cols();
+    for(auto is = 0; is < nsol; is++){
+      sols.insert(is);
+    }
+  }
+  
+  std::cout<<"Exporting "<<sols.size()<<" solutions"<<std::endl;
+  for(auto isol : sols){
+    an.LoadSolution(isol);
+    vtk.Do();
+  }
+}
