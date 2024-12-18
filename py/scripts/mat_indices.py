97fc00464203034f396ec329b9e95334f9543472
Francisco Orlandini <francisco.orlandini@gmail.com>
2024-12-18 15:35:12 -0300

    feat(material): adds glass optical data

--- py/scripts/mat_indices.py
+++ py/scripts/mat_indices.py
@@ -5,6 +5,7 @@ al_data = np.array(np.genfromtxt("data/Al.dat", delimiter=' ', dtype=np.float64)
 ag_data = np.array(np.genfromtxt("data/Ag.dat", delimiter=' ', dtype=np.float64))
 az_data = np.array(np.genfromtxt("data/AZ.dat", delimiter=' ', dtype=np.float64))
 cu_data = np.array(np.genfromtxt("data/Cu1.dat", delimiter=' ', dtype=np.float64))
+glass_data = np.array(np.genfromtxt("data/glass.dat", delimiter=' ', dtype=np.float64))
 
 def al_n(wl):
     return interp1d(al_data[:,0],al_data[:,1])(wl)[()]
@@ -22,3 +23,7 @@ def cu_n(wl):
     return interp1d(cu_data[:,0],cu_data[:,1])(wl)[()]
 def cu_k(wl):
     return interp1d(cu_data[:,0],cu_data[:,2])(wl)[()]
+def glass_n(wl):
+    return interp1d(glass_data[:,0],glass_data[:,1])(wl)[()]
+def glass_k(wl):
+    return interp1d(glass_data[:,0],glass_data[:,2])(wl)[()]
