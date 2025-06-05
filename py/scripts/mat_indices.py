21438db85f0f15283a5e4e0926008e2fc3dae416
Francisco Orlandini <francisco.orlandini@gmail.com>
2025-06-05 17:45:27 -0300

    feat(py.scripts.data): adds PVA and C8H8 refractive indices

--- py/scripts/mat_indices.py
+++ py/scripts/mat_indices.py
@@ -7,6 +7,8 @@ az_data = np.array(np.genfromtxt("data/AZ.dat", delimiter=' ', dtype=np.float64)
 cu_data = np.array(np.genfromtxt("data/Cu1.dat", delimiter=' ', dtype=np.float64))
 glass_data = np.array(np.genfromtxt("data/glass.dat", delimiter=' ', dtype=np.float64))
 TiO2_data = np.array(np.genfromtxt("data/TiO2.dat", delimiter=' ', dtype=np.float64))
+PVA_data = np.array(np.genfromtxt("data/PVA.dat", delimiter=' ', dtype=np.float64))
+C8H8_data = np.array(np.genfromtxt("data/C8H8.dat", delimiter=' ', dtype=np.float64))
 
 def al_n(wl):
     return interp1d(al_data[:,0],al_data[:,1])(wl)[()]
@@ -32,3 +34,11 @@ def TiO2_n(wl):
     return interp1d(TiO2_data[:,0],TiO2_data[:,1])(wl)[()]
 def TiO2_k(wl):
     return interp1d(TiO2_data[:,0],TiO2_data[:,2])(wl)[()]
+def PVA_n(wl):
+    return interp1d(PVA_data[:,0],PVA_data[:,1])(wl)[()]
+def PVA_k(wl):
+    return interp1d(PVA_data[:,0],PVA_data[:,2])(wl)[()]
+def C8H8_n(wl):
+    return interp1d(C8H8_data[:,0],C8H8_data[:,1])(wl)[()]
+def C8H8_k(wl):
+    return interp1d(C8H8_data[:,0],C8H8_data[:,2])(wl)[()]
